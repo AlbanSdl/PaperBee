a2d30b07b15d669db773e2a542ac20e2552ee78e
AlbanSdl <alban.delavoreille@free.fr>
2020-11-08 18:45:22 +0100

    App is now resized when keyboard is opened

--- app/src/main/java/fr/asdl/paperbee/view/FragmentContainer.kt
+++ app/src/main/java/fr/asdl/paperbee/view/FragmentContainer.kt
@@ -11,6 +11,9 @@ import androidx.core.view.children
  * With a [FragmentContainer] the fragments will all receive the WindowInsets, including the
  * vanishing fragment and the incoming one.
  * The fragments are NOT re-measured to apply changes (re-measure children when they are modified).
+ *
+ * Note: the keyboard size is applied on this element when opened ! Don't use several
+ * [FragmentContainer] in a single ViewGroup !
  */
 class FragmentContainer(context: Context, attributeSet: AttributeSet) :
     FrameLayout(context, attributeSet), WindowInsetConsumer {
@@ -24,6 +27,9 @@ class FragmentContainer(context: Context, attributeSet: AttributeSet) :
         }
         return this.useWindowInsets(
             insets,
+            action = { inset ->
+                this.setPadding(this.paddingLeft, this.paddingTop, this.paddingRight, inset.systemWindowInsetBottom)
+            },
             consumption = if (consumed) WindowInsetConsumption.SYSTEM_WINDOW else null,
             updateMeasure = false
         )
